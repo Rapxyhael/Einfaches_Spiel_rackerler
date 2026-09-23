d75cbb9be65c6c354a46c8afaaff56958694d59b
Rapxyhael <ackerlerraphael@pm.me>
2026-09-23 21:31:24 +0200

    Die Klasse wurde mit ihren Attributen erstellt und der Konstruktor erfüllt seine Aufgabe.

---
+++ src/GewinnModel.java
@@ -0,0 +1,14 @@
+/**
+ *
+ */
+public class GewinnModel {
+
+    private int gesamtPunkte;
+    private int spielerZahl;
+    private int computerZahl;
+    private int rundenErgebnis;
+
+    public GewinnModel(){
+        this.gesamtPunkte = 30;
+    }
+}
